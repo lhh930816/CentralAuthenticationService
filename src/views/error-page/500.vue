4bec4e1201e21c5b987b9d4b67623194271a7dcd
xk <1301990148@qq.com>
2020-03-04 12:12:42 +0800

    500页面

---
+++ src/views/error-page/500.vue
@@ -0,0 +1,25 @@
+<template>
+    <div class="errPage-container">
+         <div class="pic-500">
+             <img src="@/assets/500.png" alt="500">
+         </div>
+         <div class="error-handle">
+          <router-link to="/">
+            <el-button type="primary" size="large">返回首页</el-button>
+          </router-link>
+          <el-button class="error-btn" type="primary" size="large" @click="goBack">返回上一页</el-button>
+      </div>
+    </div>
+</template>
+<script>
+export default {
+    methods: {
+        goBack(){
+            this.$router.go(-1);
+        }
+    }
+}
+</script>
+<style lang="scss">
+
+</style>
